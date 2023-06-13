a1f0a1a0114e4f9a4ce70610c4b873a36449e062
Axel Daniel <axeelleguero@gmail.com>
2023-06-12 22:44:27 -0300

    Dia 2 nivel 3 completado

--- practica-js/day02/day02.js
+++ practica-js/day02/day02.js
@@ -108,7 +108,22 @@ console.log(letra);//10
 console.log("1\t1\t1\t1\t1\n2\t1\t2\t4\t8\n3\t1\t3\t9\t27\n4\t1\t4\t16\t64\n5\t1\t5\t25\t125");//11
 console.log("You cannot end a sentence with because because because is a conjunction".substr(30,24));//12
 
+//Level 3
+/*Exercises: Level 3
+'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
 
+Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+
+    const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'*/
+
+
+let longText = 'Love is the best thing in this world. Some found their love and some are still looking for their love.';
+console.log(longText.match("love"));//1
+let longText2 = 'You cannot end a sentence with because because because is a conjunction';
+console.log(longText2.match(/because/gi));//2
 
 
 
